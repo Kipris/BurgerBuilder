8c6856d2baae5b6eaf4cf792ae0031ad5b9e61c9
flaminika <koharu1993@gmail.com>
2020-03-31 19:54:10 +0300

    implementing the backdrop component

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,11 +2,7 @@ import React from 'react';
 import classes from './Backdrop.module.scss';
 
 const backdrop = (props) => {
-    return (
-        <div className={classes.Backdrop}>
-            {props.children}
-        </div>
-    );
+    return props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null;
 }
  
 export default backdrop;
