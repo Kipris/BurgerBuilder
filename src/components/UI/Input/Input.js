6eb68bbb990f8000927851f3ce94615fa6566355
flaminika <koharu1993@gmail.com>
2020-04-16 16:42:04 +0300

    adding a select component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -18,6 +18,21 @@ const input = (props) => {
                 value={props.value}
                 placeholder="" />;
             break;
+        case ('select'):
+            inputElement = (
+                <select 
+                    className={classes.Input}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option 
+                            key={option.value}
+                            value={option.value}>
+                                {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
         default:
             inputElement = <input 
                 className={classes.Input} 
