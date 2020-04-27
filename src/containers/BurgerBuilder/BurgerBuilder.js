d612bd0e86f8d598dc73236739d90b8abfbe0c49
flaminika <koharu1993@gmail.com>
2020-04-27 18:49:54 +0300

    adding tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,7 +10,7 @@ import withErrorHandler from '../../hoc/withErrorHandler';
 import axios from '../../axios-orders';
 import * as actionCreators from '../../store/actions/index';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     state = {
         purchasing: false
     }
