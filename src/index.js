e3a978034697ab76e567dc0a6488277bd3016c1d
Kipris <tnzaria@gmail.com>
2020-04-30 15:33:48 +0300

    moving the burger builder side effects into saga

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
-import { watchAuth } from './store/sagas/index';
+import { watchAuth, watchBurgerBuilder } from './store/sagas/index';
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
@@ -29,6 +29,7 @@ const store = createStore(
   composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
 
 sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -6,10 +6,15 @@ import {
     checkAuthTimeoutSaga, 
     authSaga, 
     authCheckStateSaga } from './auth';
+import { initIngredientsSaga } from './burgerBuilder';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, authLogoutSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
     yield takeEvery(actionTypes.AUTH_USER, authSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+}
+
+export function* watchBurgerBuilder() {
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
 }
